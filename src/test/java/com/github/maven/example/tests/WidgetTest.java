5d06514098bf925f7ea56f98f2bb37768eec28f8
Anindita Mozumder <Anindita@Aninditas-MacBook-Pro.local>
2016-11-25 16:11:53 -0500

    large change

--- src/test/java/com/github/maven/example/tests/WidgetTest.java
+++ src/test/java/com/github/maven/example/tests/WidgetTest.java
@@ -32,4 +32,20 @@ public class WidgetTest {
 		widget.setEnabled(true);
 		assertTrue(widget.isEnabled());
 	}
+
+	/**
+	 * Test enabling and disabling a {@link Widget}
+	 */
+	@Test
+	public void updateEnableState2() {
+		int i = 10;
+		int j = 20;
+		int x = 30;
+		int y = 60;
+		Widget widget = new Widget();
+		widget.setEnabled(false);
+		assertFalse(widget.isEnabled());
+		widget.setEnabled(true);
+		assertTrue(widget.isEnabled());
+	}
 }
